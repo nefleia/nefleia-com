da3c0b013dfd5a6fc84bbb41cbf543310eb5d5ff
taco-tortilla <tortillataco0@gmail.com>
2025-05-31 16:08:03 +0900

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    plugins: ['import', 'react', '@typescript-eslint', 'unused-imports'],
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'import/newline-after-import': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: 'next/**',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'parent',
+              position: 'before',
+            },
+            {
+              pattern: '**\\.css',
+              group: 'object',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: [
+            'builtin',
+            'external',
+            'object',
+            'type',
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+        },
+      ],
+    },
+  }),
+];
+
+export default eslintConfig;
